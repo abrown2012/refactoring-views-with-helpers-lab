52cdd49006658125965c762a6bc4d031949c5f62
Agnieszka Brown <abrown2012@icloud.com>
2021-03-28 11:14:50 -0400

    commit tree 319c879c538c3e473bfa9992894f9fa24f2fdfda

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,13 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    self.artist.name
   end
 
-  def artist_name=(name)
+  def artist_name=(n)
+      art = Artist.find_or_create_by(name: n)
+      self.artist = art
+      art.save
   end
+  
 end
